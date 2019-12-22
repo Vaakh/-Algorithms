7568507f2dccc4d2fb4c8002a360b8fef4801811
VaakhTechPriest <vahlik1234@outlook.com>
2019-12-22 14:58:56 +0300

    Today done basic threads

---
+++ Algorithms/Algorithms/LearnThreds.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class LearnThreads
+    {
+        //this class contains Thread constructor
+        public string name = "anotherThread";
+        public int nFrom = 0;
+        public int nTo = 15;
+        int k = 0;
+
+        public LearnThreads(string a, int b, int c)
+        {
+            name = a;
+            if (b >= c)
+            {
+                nFrom = c;
+                nTo = b;
+            }
+            else
+            {
+                nFrom = b;
+                nTo = c;
+            }
+        }
+
+        public void doThread()
+        {
+            
+            Thread thread = new Thread(this.testCounts);
+            thread.Name = name;
+            thread.Start(5);
+        }
+
+
+        void testCounts(object s)
+        {
+            Console.WriteLine(s);
+            for (int i = nFrom; i < nTo; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + k);
+                k++;
+            }
+
+            Console.WriteLine(Thread.CurrentThread.Name + " завершил работу");
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LearnThreads thred1 = new LearnThreads("Qwerty01", 1, 24);
+            LearnThreads thred2 = new LearnThreads("Qwerty02", 2, 22);
+
+            thred1.doThread();
+            thred2.doThread();
+
+            Console.ReadKey();
+        }
+    }
+}
